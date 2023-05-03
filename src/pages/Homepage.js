b50385383a9b3cd4b16269f3afaa26c723463c85
Alp <alpersyuleyman2002@gmail.com>
2023-05-03 22:33:26 +0200

    updated teh api to show all pokemon

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,50 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Row, Col } from "react-bootstrap";
 
 // Components
-import Pokemon from '../components/Pokemon';
-import Loader from '../components/Loader';
+import Pokemon from "../components/Pokemon";
+import Loader from "../components/Loader";
 
 const Homepage = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [pokemon, setPokemon] = useState([]);
-    const [loading, setLoading] = useState(true)
-
-    const getPokemonList = async () => {
-        let pokemonArray = [];
-        for(let i = 1; i <= 151; i ++){
-            pokemonArray.push(await getPokemonData(i));
-        }
-        console.log(pokemonArray);
-        setPokemon(pokemonArray);
-        setLoading(false);
+  const getPokemonList = async () => {
+    let pokemonArray = [];
+    for (let i = 1; i <= 800; i++) {
+      pokemonArray.push(await getPokemonData(i));
     }
+    console.log(pokemonArray);
+    setPokemon(pokemonArray);
+    setLoading(false);
+  };
 
-    const getPokemonData = async (id) => {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        return res;
-    }
+  const getPokemonData = async (id) => {
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    return res;
+  };
 
-    useEffect(() => {
-        getPokemonList();
-    }, [])
+  useEffect(() => {
+    getPokemonList();
+  }, []);
 
-    return (
-        <>
-        {loading ? (
-            <Loader/>
-        ) : (
-            <Row>
-                {pokemon.map( p =>(
-                    <Col key={p.data.name} xs={12} sm={12} md={4} lg={4} xl={4}>
-                        <Pokemon pokemon={p.data}/>
-                    </Col>
-                ))}
-            </Row>
-        )}
-        </>
-    )
-}
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Row>
+          {pokemon.map((p) => (
+            <Col key={p.data.name} xs={12} sm={12} md={4} lg={4} xl={4}>
+              <Pokemon pokemon={p.data} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
 
-export default Homepage
+export default Homepage;
